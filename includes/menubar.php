7d0b89d84b912c2739d1f2c1ad9092cc7ece5da8
Lam <maiphuonglambh.2002@gmail.com>
2023-03-02 08:48:06 +0700

    cnpm2023

---
+++ includes/menubar.php
@@ -0,0 +1,17 @@
+<section class="menu-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="navbar-collapse collapse ">
+                    <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a href="pincode-verification.php">Đăng ký học phần</a></li>
+                            <li><a href="enroll-history.php">Lịch sử đăng ký  </a></li>
+                            <li><a href="my-profile.php">Thông tin cá nhân</a></li>
+                            <li><a href="change-password.php">Đổi mật khẩu</a></li>
+                        <li><a href="logout.php">Đăng xuất</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
